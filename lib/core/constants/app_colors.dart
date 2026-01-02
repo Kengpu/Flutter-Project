aa1a818db96d67f1407c4dbbf98252aad889eed8
Mek-Sans <eng.vireaksathya@gmail.com>
2026-01-02 19:12:59 +0700

    app color

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryOrange = Color(0xFFF05D00);
+  static const Color primaryCyan = Color(0xFF00E9EF);  
+
+  static const Color background = Color(0xFF121212);    
+  static const Color surface = Color(0xFF1E1E1E);       
+  static const Color surfaceLight = Color(0xFF2C2C2C);  
+
+  static const Color success = Color(0xFF4CAF50);       
+  static const Color error = Color(0xFFE53935);         
+  static const Color starGold = Color(0xFFFFD700);      
+
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFB3B3B3);
+}
